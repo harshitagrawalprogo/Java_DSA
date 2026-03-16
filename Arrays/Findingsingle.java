072d9768ccf519dc1c1879bbbb4ffb56ee49851c
harshitagrawal-indpro <harshitagra8092@gmail.com>
2026-03-16 19:10:40 +0530

    single number

---
+++ Arrays/Findingsingle.java
@@ -0,0 +1,22 @@
+package Arrays;
+import java.util.*;
+
+class findingsingle{
+    public static void main(String[] args){
+        int arr[]= {1,6,4,5,6,1,5};
+        
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int ele:arr){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+
+        for (int ele: map.keySet()){
+            if (map.get(ele)==1){
+                System.err.println(ele);
+                break;
+            }
+        }
+    }
+}
